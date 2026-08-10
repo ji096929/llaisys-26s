1ca10ac2a9d1afdfaf065d7b442b2f8ef2877295
ji096929 <2455742327@qq.com>
2026-08-10 15:35:52 +0800

    feat:implement 2.1 argmax

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -1,7 +1,28 @@
 #include "op.hpp"
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
 
+#include "cpu/argmax_cpu.hpp"
 namespace llaisys::ops {
 void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(max_idx, max_val, vals);
+    CHECK_SAME_DTYPE(max_val->dtype(), vals->dtype());
+    CHECK_ARGUMENT(max_idx->dtype() == LLAISYS_DTYPE_I64, "argmax: max_idx must be i64"); // max_idx 固定是 i64
+    CHECK_ARGUMENT(vals->numel() > 0, "argmax: vals must not be empty");
+    ASSERT(vals->isContiguous() && max_idx->isContiguous() && max_val->isContiguous(),
+           "argmax: all tensors must be contiguous.");
+    llaisys::core::context().setDevice(max_idx->deviceType(), max_idx->deviceId());
+
+    switch (max_idx->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::argmax(max_idx->data(), max_val->data(), vals->data(),  vals->dtype(),vals->numel());
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
